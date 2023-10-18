dbe203fbd0a8f107cfb59d1fbb899df58883093d
itscool2b <93061248+itscool2b@users.noreply.github.com>
2023-10-18 18:37:35 -0400

    Delete soccer directory

--- soccer/application/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-urls = [
-    path('', views.starting_page, name='startingpage'),
-    path('login', views.login, name='login'),
-    path('signup', views.sign_up, name='signup'),
-    
-]
