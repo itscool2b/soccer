d6da8070631be1a784ca35487c32e7ca6f33dd10
itscool2b <93061248+itscool2b@users.noreply.github.com>
2023-10-18 20:19:10 -0400

    Update views.py

--- OfficalSoccerApp/application/views.py
+++ OfficalSoccerApp/application/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-
+from .forms import DashboardStatsForm, StatsPerGameForm, PlayerForm, UserForm
+from .models import CustomUser
 # Create your views here.
-def test(request):
-    pass
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        username = form.cleaned_data['username']
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        
+        if CustomUser.objects.filter(username=username).exists():
+            message.error(request,"This username is already take")
+    else:
+        form = UserForm
+    
+    return
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return
+    return
+
+def logout(request):
+    logout(request)
+    return
+
+
+
