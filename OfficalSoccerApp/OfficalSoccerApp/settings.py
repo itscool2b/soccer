5fc705aee7d6a5a949f21d747c8b18a6babfd6b7
itscool2b <itscool2@outlook.com>
2023-10-26 17:48:21 -0400

    fix

--- OfficalSoccerApp/OfficalSoccerApp/settings.py
+++ OfficalSoccerApp/OfficalSoccerApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'OfficalSoccerApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'my-react-app/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'your_app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'my-react-app/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'application/static'),  # Add this line
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Change this as needed
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
