fa1d9739c6f1bcf92759094b43f4e4d8ab5b9830
itscool2b <itscool2@outlook.com>
2023-10-26 20:52:19 -0400

    finally

--- OfficalSoccerApp/application/urls.py
+++ OfficalSoccerApp/application/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
-urlpatterns = [
 
+urlpatterns = [
     path('signup/', views.usersignup, name='signup'),
     path('login/', views.userlogin, name='login'),
     path('logout/', views.userlogout, name='logout'),
-    path("StatsPerGame/", views.StatsPerGame, name='StatsPerGame'),
-    path("DashboardStats/", views.DashboardStats, name='DashboardStats'),
-    path("GameDetail/<int:id>/", views.GameDetail, name='GameDetail'),
-    path("PlayerDetail/<int:id>/", views.PlayerDetail, name='Player')
+    path('dashboard/stats/', views.dashboard_stats_view, name='dashboard_stats'),
+    path('stats/per/game/', views.stats_per_game_view, name='stats_per_game'),
+    path('dashboard/stats/update/', views.dashboard_stats_update, name='dashboard_stats_update'),
+    path('games/', views.all_games, name='all_games'),
+    path('game/<int:id>/', views.game_detail, name='game_detail'),
+    path('players/', views.all_players, name='all_players'),
+    path('player/<int:id>/', views.player_detail, name='player_detail'),
 ]
--- OfficalSoccerApp/application/views.py
+++ OfficalSoccerApp/application/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import render
-from .forms import DashboardStatsForm, StatsPerGameForm, PlayerForm, UserForm
-from .models import CustomUser
+from django.shortcuts import render, redirect
+from .forms import UserForm, DashboardStatsForm, StatsPerGameForm
+from .models import CustomUser, DashboardStats, StatsPerGame, Player
+
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
-# Create your views here.
-from .models import Player, StatsPerGame
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
+# User registration view
+@require_http_methods(["GET", "POST"])
 def usersignup(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
-        username = form.cleaned_data['username']
         if form.is_valid():
-            if CustomUser.objects.filter(username=username).exists():
-                messages.error(request,"This username is already take")
-            else:
-                user = form.save()
-                login(request, user)
+            user = form.save()
+            login(request, user)
+            return redirect('some_redirect_url')  # Replace with your desired redirect URL
         else:
-            messages.error(request, "try again something went wrong")
-        
-    return
+            messages.error(request, "Try again, something went wrong")
+    return render(request, 'signup.html', {'form': UserForm()})
 
+# User login view
+@require_http_methods(["GET", "POST"])
 def userlogin(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -28,17 +29,21 @@ def userlogin(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return
+            return redirect('some_redirect_url')  # Replace with your desired redirect URL
         else:
-            messages.error(request, "something went wrong")
-    return
+            messages.error(request, "Invalid username or password")
+    return render(request, 'login.html')
 
+# User logout view
+@login_required
 def userlogout(request):
     logout(request)
-    return
+    return redirect('some_redirect_url')  # Replace with your desired redirect URL
 
+# DashboardStats creation view
 @login_required
-def DashboardStats(request):
+@require_http_methods(["GET", "POST"])
+def dashboard_stats_view(request):
     if request.method == 'POST':
         info = DashboardStatsForm(request.POST)
         if info.is_valid():
@@ -47,11 +52,13 @@ def DashboardStats(request):
             temp.save()
             messages.success(request, "Data saved successfully")
         else:
-            messages.error(request, "Invalid information. Please check the form.")
-    return
+            messages.error(request, "Invalid information. Please check the form")
+    return render(request, 'dashboard_stats.html', {'form': DashboardStatsForm()})
 
+# StatsPerGame creation view
 @login_required
-def StatsPerGame(request):
+@require_http_methods(["GET", "POST"])
+def stats_per_game_view(request):
     if request.method == 'POST':
         info = StatsPerGameForm(request.POST)
         if info.is_valid():
@@ -60,37 +67,39 @@ def StatsPerGame(request):
             stats_per_game.save()
             messages.success(request, "Data saved successfully")
         else:
-            messages.error(request, "invalid info")
-    return
+            messages.error(request, "Invalid information. Please check the form")
+    return render(request, 'stats_per_game.html', {'form': StatsPerGameForm()})
 
-    
-
-
+# DashboardStats update view
 @login_required
-def DashboardStatsUpdate(request):
-    instance = DashboardStats.objects.get(user=request.user)
+@require_http_methods(["GET", "POST"])
+def dashboard_stats_update(request):
+    instance, created = DashboardStats.objects.get_or_create(user=request.user)
     if request.method == 'POST':
         form = DashboardStatsForm(request.POST, instance=instance)
         if form.is_valid():
             form.save()
             messages.success(request, "Data updated successfully")
         else:
-            messages.error(request,"Error")
-    
-    else:
-        form = DashboardStatsForm(instance=instance)
+            messages.error(request, "Error")
+    return render(request, 'dashboard_stats_update.html', {'form': DashboardStatsForm(instance=instance)})
 
-def AllGames(request):
+# AllGames view
+def all_games(request):
     games = StatsPerGame.objects.all()
-    return render(request, 'index.html', games)
+    return render(request, 'index.html', {'games': games})
 
-def GameDetail(request, id):
+# GameDetail view
+def game_detail(request, id):
     game = StatsPerGame.objects.get(pk=id)
-    return
+    return render(request, 'game_detail_template.html', {'game': game})
 
-def AllPlayers(request):
-    all = Player.objects.all()
-    return
+# AllPlayers view
+def all_players(request):
+    all_players = Player.objects.all()
+    return render(request, 'player_list_template.html', {'players': all_players})
 
-def PlayerDetail(request,id):
+# PlayerDetail view
+def player_detail(request, id):
     player = Player.objects.get(pk=id)
+    return render(request, 'player_detail_template.html', {'player': player})
